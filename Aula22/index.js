f3bbd301cce51bdaef5a98cf741f258b081e8745
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2022-10-14 14:44:25 -0300

    criando aula 21 e 22

---
+++ Aula22/index.js
@@ -0,0 +1,13 @@
+const and = true && true && true && true;
+const or = true || false;
+const not = !true;
+
+console.log(and);
+console.log(or);
+console.log(not);
+
+/*const usuario = 'Japa';
+const senha = '1234';
+
+const logar = usuario ==='Japa' && senha ==='124';
+console.log(logar); */
