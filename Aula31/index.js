8fcfe2a74a8974121e62aea5b81aaff8543d87c2
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2022-10-24 14:33:31 -0300

    criando aula 31

---
+++ Aula31/index.js
@@ -0,0 +1,31 @@
+const verdadeira = true;
+/*let nome = 'menezes';
+var nome2 = 'Jarbas';
+
+if(verdadeira){
+    let nome = 'charles';
+    var nome2 = 'Charles'
+}
+console.log(nome, nome2);*/
+
+//console.log(nome);
+//var nome = 'Santos';
+
+//console.log(nome);
+//let nome ='Santos';
+
+function falaOi(){
+    let nome = 'largato';
+    var lindo = 'Lindo';
+
+    if(verdadeira){
+        let nome = 'pernilongo'
+        console.log(nome);
+        var lindo = 'beathful';
+
+    }
+    console.log(nome);
+    console.log(lindo);
+}
+console.log(lindo);
+falaOi();
