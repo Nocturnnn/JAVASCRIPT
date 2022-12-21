2546902eea5afd390560829670ba6e1be330ac84
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2022-12-20 21:23:58 -0300

    Aula 60: Revisão de Array

---
+++ Aula60/index.js
@@ -0,0 +1,29 @@
+const nomes = ['Joberto', 'Gomes', 'Miranda'];
+const nome = nomes.join(' ');
+
+console.log(nome);
+
+
+
+
+/*const nome = 'Roberto Nombre Carlos';
+const nomes = nome.split(' ');
+console.log(nomes);*/
+
+
+/*const nomes = ['Sergio', 'Marcos', 'James', 'Ra,os', 'Rosa'];
+const novo = nomes.slice(1, 3);
+console.log(novo);*/
+
+
+//nomes.push('João');
+//const novo = [...nomes];
+//Construtor de arrays
+//const nomes = new Array('Eduardo', 'Maria', 'Joana');
+
+//nomes[2] = 'Joao';
+//delete nomes[2];
+
+/*novo.pop()
+console.log(nomes);
+console.log(novo);*/
