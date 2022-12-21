8600b1b01702c3db5dfb9b998d9ecccdc4bd60e3
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2022-12-21 12:54:43 -0300

    criando aula 61 e 62
    Metodo splice e concatenação de arrays

---
+++ Aula61/index.js
@@ -0,0 +1,19 @@
+const numeros = [1, 2, 3, 4, 5];
+
+
+
+//push
+//numeros.splice(numeros.length,0, 6);
+
+//unshift
+numeros.splice(0,0, 6);
+
+//pop
+//const removidos = numeros.splice(-1, 1);
+
+//shift
+//const removidos = numeros.splice(-5, 1)
+
+console.log(numeros);
+
+
---
+++ Aula62/index.js
@@ -0,0 +1,8 @@
+const num1 = [1,2,3];
+const num2 = [4,5,6];
+
+//const num3 = num1.concat(num2);
+const num3 = [...num1, ...num2];
+
+
+console.log(num3);
