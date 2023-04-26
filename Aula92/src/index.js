ad2a811185ae471eb2c091557004f29345d6e15c
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-04-26 17:47:10 -0300

    Aula sobre Import e Export

---
+++ Aula92/src/index.js
@@ -0,0 +1,4 @@
+import multiplica, {nome as n, sobrenome, idade, soma} from './modulo1';
+
+console.log(multiplica(1,2));
+console.log(n, sobrenome, idade, soma(4,4));
---
+++ Aula92/src/modulo1.js
@@ -0,0 +1,20 @@
+ const nome = 'Carl';
+ const sobrenome = 'Miranda';
+ const idade = 30;
+const cpf = '123241421';
+
+export  function soma (x,y){
+    return x+y;
+}
+
+export default (x, y) => x * y;
+
+export { nome, sobrenome, idade}
+
+// export class Pessoa{
+//     constructor(nome,sobrenome){
+//         this.nome = nome;
+//         this.sobrenome = sobrenome;
+//     }
+// }
+
