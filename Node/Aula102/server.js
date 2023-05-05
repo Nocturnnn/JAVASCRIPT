94a1e34d797cd460f5c25b7d78491445a4a5c36d
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-05-05 14:25:26 -0300

    Aula sobre Nodemon

---
+++ Node/Aula102/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action ="/" method="POST">
+    Nome do cliente: <input type="text" name="nome">
+    <button>Enviar</button>
+    `);
+});
+app.post('/', (req,res) => {
+    res.send('Recebi o formulario');
+})
+app.get('/teste', (req,res) => {
+    res.send('Teste');
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
