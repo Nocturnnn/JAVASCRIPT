fe87e024b7c19723e4a2c69c8bb0ecec3b043be2
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-05-25 12:43:12 -0300

    Criando toda a parte de login

--- Projeto_Agenda/routes.js
+++ Projeto_Agenda/routes.js
@@ -10,6 +10,8 @@ route.get('/', homeController.index);
 // Rotas de login
 route.get('/login', loginController.index);
 route.get('/cadastrar', cadastroController.index);
-route.post('/login/register', cadastroController.register)
+route.post('/login/register', cadastroController.register);
+route.post('/login/logar', loginController.login);
+route.get('/login/logout', loginController.logout);
 
 module.exports = route;
