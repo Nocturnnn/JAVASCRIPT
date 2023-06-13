a9cef524c7e46c3aa52b638aecee41dba5ddd60c
W-Wag <wagner.oliv.m@gmail.com>
2023-06-13 14:35:41 -0300

    Criando a rota de Alunos

--- Api_Rest/src/controllers/HomeController.js
+++ Api_Rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Maria',
-      sobrenome: 'Das Dores',
-      email: 'Dolores@gmail.com',
-      idade: 80,
-      peso: 66,
-      altura: 1.63,
-    });
-    res.json(novoAluno);
+    res.json('Ok');
   }
 }
 
