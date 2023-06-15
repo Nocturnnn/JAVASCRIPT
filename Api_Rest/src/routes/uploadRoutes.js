e167b3b3396c1f90b45bd668e1856b7113fd3b48
W-Wag <wagner.oliv.m@gmail.com>
2023-06-15 14:11:36 -0300

    Tratando os errors do Multer

--- Api_Rest/src/routes/uploadRoutes.js
+++ Api_Rest/src/routes/uploadRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import uploadController from '../controllers/UploadController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('arquivo'), uploadController.store);
+router.post('/', uploadController.store);
 
 export default router;
