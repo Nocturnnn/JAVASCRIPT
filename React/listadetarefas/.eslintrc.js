4c3f34f268d02c7a53c3dc90a47b75299b95140a
W-Wag <wagner.oliv.m@gmail.com>
2023-06-19 15:09:39 -0300

    Configurando o editorConfig e Eslint

---
+++ React/listadetarefas/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-undef': 'off',
+  },
+};
--- React/listadetarefas/src/App.js
+++ React/listadetarefas/src/App.js
@@ -4,6 +4,5 @@ import './App.css';
 export default function App() {
   return (
     <h1>Hello World!</h1>
-  )
+  );
 }
-
