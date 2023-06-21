dea3f2db1271e1507e8d60127ebd2abb55fcae56
W-Wag <wagner.oliv.m@gmail.com>
2023-06-21 14:12:41 -0300

    Criando o evento de criar tarefas

--- React/listadetarefas/src/components/main.js
+++ React/listadetarefas/src/components/main.js
@@ -11,11 +11,25 @@ import "./main.css";
 export default class Main extends Component {
   state = {
     novaTarefa: "",
-    tarefas: ["Fazer café", "Beber água", "Estudar"],
+    tarefas: [],
   };
 
   handleChange = (e) => {
     this.setState({ novaTarefa: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    });
   };
 
   render() {
@@ -24,7 +38,11 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="form">
+        <form
+          action="#"
+          className="form"
+          onSubmit={this.handleSubmit}
+        >
           <input
             value={novaTarefa}
             onChange={this.handleChange}
@@ -40,10 +58,10 @@ export default class Main extends Component {
           {tarefas.map((tarefa) => (
             <li key={tarefa}>
               {tarefa}
-              <div>
+              <span>
                 <FaEdit className="edit" />
                 <FaWindowClose className="delete" />
-              </div>
+              </span>
             </li>
           ))}
         </ul>
