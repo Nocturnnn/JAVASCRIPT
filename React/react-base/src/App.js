7243aee6b55a4d4c39596383c0580433b30be33a
W-Wag <wagner.oliv.m@gmail.com>
2023-06-28 13:52:14 -0300

    Trocando BrowserRouter por Route e History é  Criando MyRoute

--- React/react-base/src/App.js
+++ React/react-base/src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router history={history}>
         <GlobalStyles />
         <Header />
         <Routes />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
