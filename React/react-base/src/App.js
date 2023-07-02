03fcb34ae97f7fe58ed7874140c72be48efa6f5f
W-Wag <wagner.oliv.m@gmail.com>
2023-07-02 13:36:02 -0300

    Criando o persist de dados com Redux Persist

--- React/react-base/src/App.js
+++ React/react-base/src/App.js
@@ -1,8 +1,9 @@
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store';
+import store, { persistor } from './store';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
@@ -12,12 +13,14 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Router history={history}>
-          <GlobalStyles />
-          <ToastContainer autoClose={3000} className="toast-container" />
-          <Header />
-          <Routes />
-        </Router>
+        <PersistGate persistor={persistor}>
+          <Router history={history}>
+            <GlobalStyles />
+            <ToastContainer autoClose={3000} className="toast-container" />
+            <Header />
+            <Routes />
+          </Router>
+        </PersistGate>
       </Provider>
     </div>
   );
