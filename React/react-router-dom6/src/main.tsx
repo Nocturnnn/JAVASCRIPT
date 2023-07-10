db8ead2af649897b5a12e70bd85f03f715d96910
W-Wag <wagner.oliv.m@gmail.com>
2023-07-10 17:11:12 -0300

    Diferenças entre Tag A e Link

--- React/react-router-dom6/src/main.tsx
+++ React/react-router-dom6/src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { About } from './components/About'
 import './styles/global.css'
+import { Menu } from './components/Menu'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <BrowserRouter>
+    <Menu /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
